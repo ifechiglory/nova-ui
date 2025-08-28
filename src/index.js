8ef4ef7735519fb02d92477a41d4046d4389b77f
Ifechukwu Edet <ifechiglory@gmail.com>
2025-08-28 11:49:02 +0100

    update package name

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// src/index.js
+export { default as Button } from "./components/Button/Button";
+export { default as Card } from "./components/Card/Card";
+export { default as Input } from "./components/Input/Input";
+export { default as Badge } from "./components/Badge/Badge";
+export { default as Modal } from "./components/Modal/Modal";
+export { default as Alert } from "./components/Alert/Alert";
